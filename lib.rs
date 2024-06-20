ae1d536b2e4530783a07e68bd06934dcc7175b32
MRmateoricci <riccimatu@gmail.com>
2024-06-20 16:23:17 -0300

    com

--- lib.rs
+++ lib.rs
@@ -4,7 +4,6 @@
 mod sistema {
     use ink::prelude::vec::Vec;
     use ink::prelude::string::String;
-    
 
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     #[cfg_attr(
@@ -14,7 +13,7 @@ mod sistema {
     #[derive(Debug)]
     pub enum Rol{
         Votante,
-        Candidato(String),
+        Candidato,
     }
 
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
@@ -30,26 +29,71 @@ mod sistema {
         dni:i32,
         verificado:bool,
         rol:Option<Rol>,
-        voto:bool,
         acc_id:AccountId
     }
     impl Usuario{
 
         pub fn new(nombre:String,apellido:String,dni:i32,edad:i32,verificado:bool,rol:Option<Rol>,acc_id:AccountId)->Self{
-            Self{nombre,apellido,dni,edad,verificado,rol,voto:false,acc_id}
+            Self{nombre,apellido,dni,edad,verificado,rol,acc_id}
         }
     }
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
+    #[cfg_attr(
+        feature = "std",
+        derive(ink::storage::traits::StorageLayout)
+    )]
+    #[derive(Debug)]
+    pub struct Fecha{
+        pub dia:i32,
+        pub mes:i32,
+        pub anio:i32
+    }
+
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
+    #[cfg_attr(
+        feature = "std",
+        derive(ink::storage::traits::StorageLayout)
+    )]
+    #[derive(Debug)]
+    struct Votacion{
+        id:i32,
+        puesto:String,
+        candidatos:Vec<Usuario>,
+        votos: Vec<u32>,    // hashmap con accountid de candidato
+        votaron: Vec<AccountId>,
+        fecha_inicio:Fecha,
+        fecha_fin:Fecha,
+    }
+    impl Votacion{
+        pub fn new(id:i32,puesto:String, fecha_inicio:Fecha, fecha_fin:Fecha)-> Votacion{
+            Votacion {
+                id, puesto, candidatos:Vec::new(), votos:Vec::new(), votaron:Vec::new(),fecha_inicio, fecha_fin
+            }
+        }
+
+        pub fn inicio(&self)->bool{
+            true
+        }
+
+        pub fn finalizo(&self)->bool{
+           true 
+        }
+
+
+    }
+
+
+    
     #[ink(storage)]
     pub struct Sistema {
         nombre_administrador:String,
-        usuarios_reg: Vec<Usuario>,
+        usuarios_reg: Vec<Usuario>, // hashmap con account id
+        espera:Vec<AccountId>,
+        votaciones:Vec<Votacion>,  // hashmap con id de votacion
         votantes: Vec<Usuario>,
         candidatos: Vec<Usuario>,
-        admin:AccountId
+        admin:AccountId,
     }
     
 
@@ -57,7 +101,7 @@ mod sistema {
         
         #[ink(constructor)]
         pub fn new(nombre_administrador: String) -> Self {
-            Self { nombre_administrador, usuarios_reg:Vec::new(),votantes:Vec::new(),candidatos:Vec::new(), admin: Self::env().caller() }
+            Self { nombre_administrador,espera:Vec::new(), usuarios_reg:Vec::new(),votaciones: Vec::new(),votantes:Vec::new(),candidatos:Vec::new(), admin: Self::env().caller() }
         }
 
         #[ink(message)]
@@ -70,20 +114,28 @@ mod sistema {
             }
         }
 
+        #[ink(message)]
+        pub fn crear_votacion(&mut self, id:i32, puesto:String,fecha_inicio:Fecha,fecha_fin:Fecha){
+            let caller = self.env().caller();
+            if caller == self.admin {
+                let v = Votacion::new(id, puesto, fecha_inicio, fecha_fin);
+                self.votaciones.push(v);
+            }
+        }
+
 
         #[ink(message)]
-        pub fn postularse_como_candidato(&mut self, nombre_de_partido:String){
+        pub fn postularse_como_candidato(&mut self,id_de_votacion:i32){
             let caller = self.env().caller();
             self.usuarios_reg.iter_mut().for_each(|u|{
                 if u.acc_id==caller {
-                    if !u.verificado{
-                        u.rol = Some(Rol::Candidato(nombre_de_partido.clone()));
-                        u.verificado=true;
+                    if !u.verificado && !self.espera.contains(&caller){
+
+                        u.rol = Some(Rol::Candidato);
+                        self.espera.push(caller);
                     }   
                 }
             })
-
-            
         }
 
         #[ink(message)]
